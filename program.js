a1bf39c7ac0ce07db1fece55b2324cadfbe675b7
PatrickHalpin <patrickhalpin@patricks-MacBook-Air.local>
2016-01-26 14:18:13 +0000

    update 2

--- program.js
+++ program.js
@@ -1,11 +1,23 @@
 var fs = require("fs");
 
+//getting the contents of the file
 fs.readdir("./", function (err, files)
 {
 	console.log("Got contents of the directory");
+	//searching through conetents and getting the names of each file to be read out
 	for(var x in files)
-	 {
-		console.log(files[x]);
+	 {	
+	 	//testing if the file name is the same as the program name
+	 	if(files[x]!='program.js')
+	 	{
+			console.log(files[x]);
+			//read the file and print out the conents 
+			fs.readFile(x, function (err, data)
+			{
+				console.log(data);		
+			});
+		}
+
 		
 		
 	}
